8b57223862a35153c957f822bf1340af07f7e324
STerliakov <terlya.stas@gmail.com>
2025-01-16 04:56:31 +0100

    Retrieving issues

--- src/mypy_issues/__init__.py
+++ src/mypy_issues/__init__.py
@@ -1,5 +1,15 @@
 from __future__ import annotations
 
+import os
+
+from githubkit import GitHub
+
 
 def main() -> None:
-    print("Hello from mypy-issues!")
+    token = os.getenv("GH_ACCESS_TOKEN")
+    assert token is not None, "Please pass a PAT"
+    gh = GitHub(token)
+    issues = gh.rest.issues.list_for_repo(
+        "python", "mypy", state="open", sort="created", direction="desc", per_page=50
+    ).parsed_data
+    print(issues)
